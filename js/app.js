4dfdba40491815be4d8a7d92559e49e940cee71d
feanorfeanturi <feanorfeanturi@gmail.com>
2016-03-21 01:51:02 +0000

    new features problem with show\hide and columns

--- js/app.js
+++ js/app.js
@@ -1,40 +1,59 @@
+/*global angular*/
 "use strict"; // да, да в ES6 встроен строгий режим по умолчанию,
             //но ведь я буду писать на ES5(по крайней мере я так думаю)
             // и нет, я не буду в этот раз ковыряться с ие8
 
-
+//сделал пока простыней. не вижу смысла размазывать код на текущем этапе
 
 var pokedexApp=angular.module("pokedexApp",['ngResource']);
 
-var pokemonApiHost="https://pokeapi.co/api/v2/";
+//мы используем первую версию апи как в тз
+//получаем весь датасет сразу
+var pokemonApiHost="https://pokeapi.co/api/v1/";
 
 pokedexApp.factory("Pokemons",function($resource){
     return $resource(pokemonApiHost+"pokemon/?limit=:limit&offset=:offset");
 });
 
-pokedexApp.controller("pokemonListController",function($scope,Pokemons){
+var pokemonListController=pokedexApp.controller("pokemonListController",function($scope,Pokemons){
+    
     $scope.noMorePokemons=false;
+    $scope.showDetailsOfPokemon=false;
+    
     $scope.pokemons=null;
+    $scope.selectedPokemon=null;
     
     $scope.nextPokemons="";
-    $scope.limit=500;
+    $scope.limit=12;
     $scope.offset=0;
+    
+    //получение базового массива покемонов в самом начале
     Pokemons.get({limit:$scope.limit, offset:$scope.offset},function(data){
-        $scope.pokemons=data.results;
-        $scope.nextPokemons=data.next;
+        $scope.pokemons=data.objects;
+        $scope.nextPokemons=data.meta.next;
         $scope.offset+=$scope.limit;
         
     });
-    
-    
+
+
+    //onclicklistener переключалка на выбранного покемона
+    //перезаписать объект и сместить фокус
+    $scope.switchToPokemon = function (pokemon) {
+        $scope.selectedPokemon=pokemon;
+        $scope.showDetailsOfPokemon=true;
+        document.getElementById('#poke-details').focus();
+    };
+
+
+    //button 
     $scope.loadMore = function () {
         try{
             Pokemons.get({limit:$scope.limit, offset:$scope.offset},function(data) {
-                if (data.next==null){
+                if (data.meta.next==null){
                     $scope.noMorePokemons=true;
                 }
                 $scope.offset+=$scope.limit;
-                $scope.pokemons=$scope.pokemons.concat(data.results);
+                $scope.pokemons=$scope.pokemons.concat(data.objects);
             });
         } catch (err)
         {
@@ -42,4 +61,18 @@ pokedexApp.controller("pokemonListController",function($scope,Pokemons){
             console.log(err);
         }
     }
+});
+
+pokemonListController.directive("pokeCard",function(){
+    return{
+        restrict:'E',
+        templateUrl: 'html/poke-card.html'
+    }
+});    
+
+pokemonListController.directive('pokeInfo',function() {
+    return {
+        restrict:'E',
+        templateUrl:'html/poke-info.html'
+    };
 });
